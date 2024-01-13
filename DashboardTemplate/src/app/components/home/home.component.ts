c3f021f1eba2a5c197c8d9513c334e8b838a8711
Cuma KÖSE <turkmvc@gmail.com>
2024-01-13 20:49:36 +0300

    Update home.component.ts

--- DashboardTemplate/src/app/components/home/home.component.ts
+++ DashboardTemplate/src/app/components/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { BlankpageComponent } from '../../ui/common/blank/blankpage.component';
+import { BlankComponent } from '../../ui/common/blank/blankpage.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BlankpageComponent],
+  imports: [BlankComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
